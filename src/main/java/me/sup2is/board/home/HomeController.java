07ee380f6dee32d39e153c1e00f54cf00e094f11
sup2is <dev.sup2is@gmail.com>
2019-10-20 23:31:50 +0900

    HomeController 생성

---
+++ src/main/java/me/sup2is/board/home/HomeController.java
@@ -0,0 +1,13 @@
+package me.sup2is.board.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
