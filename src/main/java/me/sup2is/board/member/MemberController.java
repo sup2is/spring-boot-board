306cb65b0111882c70aa81cc10b21d5b27c6912e
sup2is <dev.sup2is@gmail.com>
2019-12-06 00:38:42 +0900

    Member 저장 기능 추가

---
+++ src/main/java/me/sup2is/board/member/MemberController.java
@@ -0,0 +1,38 @@
+package me.sup2is.board.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    final private MemberService memberService;
+
+    @GetMapping("/member/create")
+    public String create(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "/member/create";
+    }
+
+    @PostMapping("/member/create")
+    public String createMember(@ModelAttribute @Valid MemberForm memberForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            //todo 넘어온 field error 매핑해서 다시 /member/create 로 보내주는 코드 필요함
+        }
+        memberService.save(memberForm.createMember());
+        return "redirect:/";
+    }
+
+
+
+
+}
--- src/main/java/me/sup2is/board/member/MemberForm.java
+++ src/main/java/me/sup2is/board/member/MemberForm.java
@@ -2,24 +2,34 @@ package me.sup2is.board.member;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import me.sup2is.board.model.Member;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@ToString
 public class MemberForm {
 
-    @NotNull(message = "패스워드를 입력해주세요.")
+    @NotBlank(message = "아이디를 입력해주세요.")
+    private String userId;
+
+    @NotBlank(message = "패스워드를 입력해주세요.")
     private String passwd;
 
-    @NotNull(message = "이름을 입력해주세요")
+    @NotBlank(message = "이름을 입력해주세요")
     private String username;
 
     @Email(message = "이메일 형식이 아닙니다. 이메일을 확인해주세요.")
-    @NotNull(message = "이메일을 입력해주세요.")
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
 
     private String zipcode;
 
+    public Member createMember() {
+        return Member.createMember(userId, passwd, username, email, zipcode);
+    }
 }
